011017e732ae07880cc9d42df1d9d15681a3d90c
Eduardo2486 <eduardo0137@protonmail.com>
2021-07-12 12:47:06 -0500

    Sidebar item artists added

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,2 +1,25 @@
-let mainImgContainer = document.getElementById("main-img-item");
-let imgsDetail = document.querySelectorAll(".img-detail-item");
+$(document).ready(function(){
+
+  /*
+    Show in the main div the image choosed for the small boxes.
+  */
+  $(".img-detail-item").click(function(){
+    let src = $(this).attr("src");
+    $(".main-img-item").attr("src", src)
+  });
+
+  /*
+    Select size
+  */
+  $(".option-box-size").click(function(){
+    if($(this).attr("class") === "option-box-size selected"){
+      $(this).attr("class", "option-box-size")
+    }else{  
+      $(".option-box-size").each(function(){
+        $(this).attr("class", "option-box-size");
+      });
+      $(this).attr("class", "option-box-size selected");
+    }
+    
+  });
+});
